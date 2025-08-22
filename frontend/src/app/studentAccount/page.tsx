d775b05891e760e71bbdb18a7faa81c210a6ae78
Joshclxx <joshuacolobong11@gmail.com>
2025-08-22 21:20:58 +0800

    Update: Update studentAccount UI with logout button

---
+++ frontend/src/app/studentAccount/page.tsx
@@ -0,0 +1,12 @@
+import StudentAccount from "@/components/students/StudentAccount";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <StudentAccount />
+    </div>
+  );
+};
+
+export default page;
